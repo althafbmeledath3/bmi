93574ffaf6a75c98e1f2f3387e7a82da98bd2e68
althafbmeledath3 <159973979+althafbmeledath3@users.noreply.github.com>
2025-03-24 17:59:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
 let a = document.getElementById('content');
 let out = document.getElementsByClassName('out');
-col = document.getElementsByClassName('color')
+let col = document.getElementsByClassName('color');
 
 function calcBMI() {
-    
     let height = document.getElementById('Height').value;
     let weight = document.getElementById('Weight').value;
 
-   
     if (height && weight) {
-       
-        height = parseFloat(height) / 100; 
-        height = height ** 2; 
+        height = parseFloat(height) / 100;
+        height = height * height; // or height = Math.pow(height, 2);
 
-       
         weight = parseFloat(weight);
 
-        // Calculate BMI
         let bmi = weight / height;
 
-        
         if (bmi < 18.5) {
             a.textContent = "Underweight"; 
             out[0].style.visibility = 'visible'; 
-            col[0].style.backgroundColor = "Yellow"
-            
+            col[0].style.backgroundColor = "Yellow";
         }
 
-
-        if (bmi=>18.5 && bmi<=24.9) {
+        if (bmi >= 18.5 && bmi <= 24.9) {
             a.textContent = "Normal"; 
             out[0].style.visibility = 'visible'; 
-            col[0].style.backgroundColor = "green"
-            
+            col[0].style.backgroundColor = "green";
         }
 
-        if (bmi=>25.9 && bmi<=29.9) {
-            a.textContent = "OverWeight"; 
+        if (bmi >= 25 && bmi <= 29.9) {
+            a.textContent = "Overweight"; 
             out[0].style.visibility = 'visible'; 
-            col[0].style.backgroundColor = "red"
-            
+            col[0].style.backgroundColor = "red";
         }
     }
 }
