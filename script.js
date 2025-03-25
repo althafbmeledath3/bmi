67285fa491b4fd51386d643f507bd0ca7fd88532
althafbmeledath3 <159973979+althafbmeledath3@users.noreply.github.com>
2025-03-25 09:16:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,29 +7,40 @@ function calcBMI() {
     let weight = document.getElementById('Weight').value;
 
     if (height && weight) {
-        height = parseFloat(height) / 100;
+        height = parseFloat(height) / 100; // Convert cm to meters
         height = height * height; // or height = Math.pow(height, 2);
-
+        
         weight = parseFloat(weight);
 
         let bmi = weight / height;
 
+        // Display results
         if (bmi < 18.5) {
-            a.textContent = "Underweight"; 
-            out[0].style.visibility = 'visible'; 
-            col[0].style.backgroundColor = "Yellow";
+            a.textContent = "Underweight";
+            out[0].style.visibility = 'visible';
+            col[0].style.backgroundColor = "yellow";
         }
 
         if (bmi >= 18.5 && bmi <= 24.9) {
-            a.textContent = "Normal"; 
-            out[0].style.visibility = 'visible'; 
+            a.textContent = "Normal";
+            out[0].style.visibility = 'visible';
             col[0].style.backgroundColor = "green";
         }
 
         if (bmi >= 25 && bmi <= 29.9) {
-            a.textContent = "Overweight"; 
-            out[0].style.visibility = 'visible'; 
+            a.textContent = "Overweight";
+            out[0].style.visibility = 'visible';
             col[0].style.backgroundColor = "red";
         }
+
+        if (bmi >= 30) {
+            a.textContent = "Obesity";
+            out[0].style.visibility = 'visible';
+            col[0].style.backgroundColor = "darkred";
+        }
+    } else {
+        a.textContent = "Please fill out both fields!";
+        out[0].style.visibility = 'visible';
+        col[0].style.backgroundColor = "grey";
     }
 }
